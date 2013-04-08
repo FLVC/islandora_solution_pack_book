cc8138b9b6431486d90adc62ebe2a7cbe1cc69c0
Gail Lewis <fclgnl@islandorad.fcla.edu>
2013-04-08 14:08:14 -0400

    Created links in ToC display.

--- theme/islandora-toc.tpl.php
+++ theme/islandora-toc.tpl.php
@@ -10,7 +10,7 @@
   <?php foreach($toc_entries['table_of_contents'] as $toc_entry): ?>
       <tr>
       <td><?php print $toc_entry['label']; ?></td>
-      <td><?php print $toc_entry['title']; ?></td>
+      <td><?php print l($toc_entry['title'], "islandora/object/{$object->id}", array('fragment' => "page/{$toc_entry['pagenum']}/mode/1up")); ?></td>
       <td><?php print $toc_entry['pagenum']; ?></td>
       </tr>
   <?php endforeach; ?>
