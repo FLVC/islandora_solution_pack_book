57f4a82bad3077e3092ff7e95088accd36627733
Gail Lewis <fclgnl@islandorad.fcla.edu>
2013-05-29 18:34:48 -0400

    Adding structured TOC display for tab.

--- theme/islandora-toc.tpl.php
+++ theme/islandora-toc.tpl.php
@@ -5,14 +5,29 @@
  */
 ?>
 <h3>Table of Contents</h3>
-<table>
-<tbody>
+<div id="toctree" class="islandora-toc">
+  <?php $lastlevel = 0 ?>
   <?php foreach($toc_entries['table_of_contents'] as $toc_entry): ?>
-      <tr>
-      <td><?php print $toc_entry['label']; ?></td>
-      <td><?php print l($toc_entry['title'], "islandora/object/{$object->id}", array('fragment' => "page/{$toc_entry['pagenum']}/mode/1up")); ?></td>
-      <td><?php print $toc_entry['pagenum']; ?></td>
-      </tr>
+      <?php $currentlevel = $toc_entry['level']; ?>
+      <?php $nodeid = "L" . $currentlevel . "P" . $toc_entry['pagenum']; ?>
+      <?php if (($currentlevel > $lastlevel)&&($currentlevel - $lastlevel == 1)): ?>
+      <?php     print "<ul><li id='$nodeid'>"; ?>
+      <?php endif; ?>
+      <?php if ($currentlevel == $lastlevel): ?>
+      <?php     print "</li><li id='$nodeid'>"; ?>
+      <?php endif; ?>
+      <?php if ($currentlevel < $lastlevel): ?>
+      <?php     for ($i=$currentlevel; $i<$lastlevel; $i++): ?>
+      <?php         print "</li></ul>"; ?>
+      <?php     endfor; ?>
+      <?php     print "</li><li id='$nodeid'>"; ?>
+      <?php endif; ?>
+      <?php print l($toc_entry['title'], "islandora/object/{$object->id}", array('fragment' => "page/{$toc_entry['pagenum']}/mode/1up")); ?>
+      <?php $lastlevel = $currentlevel ?>
   <?php endforeach; ?>
-</tbody>
-</table>
+  <?php if ($lastlevel > 0): ?>
+  <?php     for ($i=0; $i<$lastlevel; $i++): ?>
+  <?php         print "</li></ul>"; ?>
+  <?php     endfor; ?>
+  <?php endif; ?>
+</div>
