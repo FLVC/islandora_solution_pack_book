668d00b32cb8256bc7ac583d45589bd007c579b8
Gail Lewis <fclgnl@islandorad.fcla.edu>
2013-06-11 23:35:04 -0400

    Adding table of contents tab with tree display.

---
+++ theme/islandora-toc.tpl.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * @file
+ *
+ */
+?>
+<h3>Table of Contents</h3>
+<div id="toctree" class="islandora-toc">
+  <?php $lastlevel = 0 ?>
+  <?php foreach($toc_entries['table_of_contents'] as $toc_entry): ?>
+      <?php $currentlevel = $toc_entry['level']; ?>
+      <?php $nodeid = "L" . $currentlevel . "P" . $toc_entry['pagenum']; ?>
+      <?php if (($currentlevel > $lastlevel)&&($currentlevel - $lastlevel == 1)): ?>
+      <?php     print "<ul><li id='$nodeid'>"; ?>
+      <?php endif; ?>
+      <?php if ($currentlevel == $lastlevel): ?>
+      <?php     print "</li><li id='$nodeid'>"; ?>
+      <?php endif; ?>
+      <?php if ($currentlevel < $lastlevel): ?>
+      <?php     for ($i=$currentlevel; $i<$lastlevel; $i++): ?>
+      <?php         print "</li></ul>"; ?>
+      <?php     endfor; ?>
+      <?php     print "</li><li id='$nodeid'>"; ?>
+      <?php endif; ?>
+      <?php $pageindex = intval($toc_entry['pagenum']) - 1; ?>
+      <?php print l($toc_entry['title'], "islandora/object/{$object->id}", array('fragment' => "page/n{$pageindex}/mode/2up")); ?>
+      <?php $lastlevel = $currentlevel ?>
+  <?php endforeach; ?>
+  <?php if ($lastlevel > 0): ?>
+  <?php     for ($i=0; $i<$lastlevel; $i++): ?>
+  <?php         print "</li></ul>"; ?>
+  <?php     endfor; ?>
+  <?php endif; ?>
+</div>
